adff57d7b9aa51f35e718bd09f95e3fec4132592
Matías Morales <programacionfrida@gmail.com>
2023-06-04 22:08:00 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Landing, Home, Detail, Form } from "./views";
 import { Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 
-axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.baseURL = "http://localhost:3001";  //Descomentar esta linea cuando quieramos trabajar de manera local.
+axios.defaults.baseURL = "https://servidor-food-gvnp.onrender.com";  //Descomentar esta linea cuando querramos pushear/actualizar el proyecto.
+
 
 function App() {
 
